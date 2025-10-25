d8f0df11deed4cff9860400c02e9d56abfc05801
eizus <hello@cdr.xyz>
2025-10-25 11:02:27 -0400

    Write more content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -61,7 +61,7 @@ function FeatureSection() {
     <div className="overflow-hidden">
       <Container className="pb-24">
         <Heading as="h2" className="max-w-3xl">
-          A snapshot of your entire sales pipeline.
+          Rest easy, we'll grow your Substack while you sleep.
         </Heading>
         <Screenshot
           width={1216}
@@ -77,26 +77,16 @@ function FeatureSection() {
 function BentoSection() {
   return (
     <Container>
-      <Subheading>Sales</Subheading>
+      <Subheading>Automated Growth</Subheading>
       <Heading as="h3" className="mt-2 max-w-3xl">
-        Know more about your customers than they do.
+        You already have everything you need. Let us do the rest.
       </Heading>
 
       <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
         <BentoCard
-          eyebrow="Insight"
-          title="Get perfect clarity"
-          description="Radiant uses social engineering to build a detailed financial picture of your leads. Know their budget, compensation package, social security number, and more."
-          graphic={
-            <div className="h-80 bg-[url(/screenshots/profile.png)] bg-size-[1000px_560px] bg-position-[left_-109px_top_-112px] bg-no-repeat" />
-          }
-          fade={['bottom']}
-          className="max-lg:rounded-t-4xl lg:col-span-3 lg:rounded-tl-4xl"
-        />
-        <BentoCard
           eyebrow="Analysis"
-          title="Undercut your competitors"
-          description="With our advanced data mining, you’ll know which companies your leads are talking to and exactly how much they’re being charged."
+          title="Use What Works: Your Voice"
+          description="Your readers already love your writing, so give the people what they want. We automate your social media growth so your words go farther, and your Substack revenue & subscriber count goes up."
           graphic={
             <div className="absolute inset-0 bg-[url(/screenshots/competitors.png)] bg-size-[1100px_650px] bg-position-[left_-38px_top_-73px] bg-no-repeat" />
           }
@@ -104,9 +94,19 @@ function BentoSection() {
           className="lg:col-span-3 lg:rounded-tr-4xl"
         />
         <BentoCard
-          eyebrow="Speed"
-          title="Built for power users"
-          description="It’s never been faster to cold email your entire contact list using our streamlined keyboard shortcuts."
+          eyebrow="Smart Quotes"
+          title="Our Agents Bring Out Your Best"
+          description="Some writers are told to trash their social media accounts with AI slop to grow. It's not. Our AI agents choose the best quotes and selections from your long-form articles. Your feed remains purely in your voice. Always."
+          graphic={
+            <div className="h-80 bg-[url(/screenshots/profile.png)] bg-size-[1000px_560px] bg-position-[left_-109px_top_-112px] bg-no-repeat" />
+          }
+          fade={['bottom']}
+          className="max-lg:rounded-t-4xl lg:col-span-3 lg:rounded-tl-4xl"
+        />
+        <BentoCard
+          eyebrow="Automation"
+          title="Only Minutes to Set Up, Years of Automated Posts"
+          description="Upload Your Archive. Connect to X. Profit. Setting up the growth engine for your Substack doesn't have to be hard."
           graphic={
             <div className="flex size-full pt-10 pl-10">
               <Keyboard highlighted={['LeftCommand', 'LeftShift', 'D']} />
@@ -115,16 +115,16 @@ function BentoSection() {
           className="lg:col-span-2 lg:rounded-bl-4xl"
         />
         <BentoCard
-          eyebrow="Source"
-          title="Get the furthest reach"
-          description="Bypass those inconvenient privacy laws to source leads from the most unexpected places."
+          eyebrow="Share"
+          title="Find Your Future Readers Where They Are"
+          description="X. Instagram. Facebook. LinkedIn. TikTok. We get your quotes in front of your future readers, wherever they are."
           graphic={<LogoCluster />}
           className="lg:col-span-2"
         />
         <BentoCard
-          eyebrow="Limitless"
-          title="Sell globally"
-          description="Radiant helps you sell in locations currently under international embargo."
+          eyebrow="Scheduled Posts"
+          title="Be the Poaster, Without Doing any Posting"
+          description="Choose your own custom posting schedule so your feeds are always fresh with your quotes and long-form articles. "
           graphic={<Map />}
           className="max-lg:rounded-b-4xl lg:col-span-2 lg:rounded-br-4xl"
         />
@@ -137,17 +137,17 @@ function DarkBentoSection() {
   return (
     <div className="mx-2 mt-2 rounded-4xl bg-gray-900 py-32">
       <Container>
-        <Subheading dark>Outreach</Subheading>
+        <Subheading dark>Agentic AI</Subheading>
         <Heading as="h3" dark className="mt-2 max-w-3xl">
-          Customer outreach has never been easier.
+          Automated social media growth, powered by AI agents.
         </Heading>
 
         <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
           <BentoCard
             dark
-            eyebrow="Networking"
-            title="Sell at the speed of light"
-            description="Our RadiantAI chat assistants analyze the sentiment of your conversations in real time, ensuring you're always one step ahead."
+            eyebrow="Analysis"
+            title="Agents that find the viral quotes you already wrote"
+            description="Our AI Agents are trained to choose the best quotes which will go viral and grow your Substack when posted on social media."
             graphic={
               <div className="h-80 bg-[url(/screenshots/networking.png)] bg-size-[851px_344px] bg-no-repeat" />
             }
@@ -157,25 +157,25 @@ function DarkBentoSection() {
           <BentoCard
             dark
             eyebrow="Integrations"
-            title="Meet leads where they are"
-            description="With thousands of integrations, no one will be able to escape your cold outreach."
+            title="..."
+            description="..."
             graphic={<LogoTimeline />}
             // `overflow-visible!` is needed to work around a Chrome bug that disables the mask on the graphic.
             className="z-10 overflow-visible! lg:col-span-2 lg:rounded-tr-4xl"
           />
           <BentoCard
             dark
-            eyebrow="Meetings"
-            title="Smart call scheduling"
-            description="Automatically insert intro calls into your leads' calendars without their consent."
+            eyebrow="Growth"
+            title="More Paid Subscribers"
+            description="Our AI agentic powered workflows do the heavy lifting of growing your Substack, so you can focus on writing."
             graphic={<LinkedAvatars />}
             className="lg:col-span-2 lg:rounded-bl-4xl"
           />
           <BentoCard
             dark
             eyebrow="Engagement"
-            title="Become a thought leader"
-            description="RadiantAI automatically writes LinkedIn posts that relate current events to B2B sales, helping you build a reputation as a thought leader."
+            title="Set up once. Even for articles you haven't written yet."
+            description="Setup only takes minutes, and even includes future articles you haven't yet published. Just add our custom email as a paid subscriber to your Substack and we'll handle the rest."
             graphic={
               <div className="h-80 bg-[url(/screenshots/engagement.png)] bg-size-[851px_344px] bg-no-repeat" />
             }
@@ -192,17 +192,17 @@ export default function Home() {
   return (
     <div className="overflow-hidden">
       <Hero />
-      {/*<main>*/}
-      {/*  <Container className="mt-10">*/}
-      {/*    <LogoCloud />*/}
-      {/*  </Container>*/}
-      {/*  <div className="bg-linear-to-b from-white from-50% to-gray-100 py-32">*/}
-      {/*    <FeatureSection />*/}
-      {/*    <BentoSection />*/}
-      {/*  </div>*/}
-      {/*  <DarkBentoSection />*/}
-      {/*</main>*/}
-      {/*<Testimonials />*/}
+      <main>
+        {/*<Container className="mt-10">*/}
+        {/*  <LogoCloud />*/}
+        {/*</Container>*/}
+        <div className="bg-linear-to-b from-white from-50% to-gray-100 py-32">
+          <FeatureSection />
+          <BentoSection />
+        </div>
+        <DarkBentoSection />
+      </main>
+      <Testimonials />
       <Footer />
     </div>
   )
