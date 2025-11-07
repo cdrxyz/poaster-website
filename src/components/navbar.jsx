879525448120646b339879210279e3e307236b95
eizus <hello@cdr.xyz>
2025-11-07 09:50:23 -0500

    Terms, Privacy, Fixed Images

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -85,7 +85,7 @@ export function Navbar({ banner }) {
             <PlusGridItem className="py-3">
               <Link className="flex ailgn-middle" href="/" title="Home">
                 <Logo className="h-32" />
-                <span className="text-2xl font-mono">Poaster</span>
+                <span className="text-2xl font-extrabold mt-2">Poaster</span>
               </Link>
             </PlusGridItem>
             {banner && (
